644f59f18408d8c8a93aa6590bab8d8630740a36
LucasMacecna09 <macenamoreiralucas30@gmail.com>
2025-01-23 18:52:19 -0300

    Game seed

--- src/main/java/com/gameslyst/dslist/entities/Game.java
+++ src/main/java/com/gameslyst/dslist/entities/Game.java
@@ -15,10 +15,14 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
@@ -31,7 +35,7 @@ public class Game {
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = plataforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -70,12 +74,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataforms() {
-        return plataforms;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+    public void setPlatforms(String plataforms) {
+        this.platforms = plataforms;
     }
 
     public Double getScore() {
