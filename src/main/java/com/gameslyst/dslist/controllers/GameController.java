272373413aa0d3256acf70e738d7bcbc12ae3898
LucasMacecna09 <macenamoreiralucas30@gmail.com>
2025-01-24 18:31:13 -0300

    Database seed update

--- src/main/java/com/gameslyst/dslist/controllers/GameController.java
+++ src/main/java/com/gameslyst/dslist/controllers/GameController.java
@@ -1,10 +1,11 @@
 package com.gameslyst.dslist.controllers;
 
+import com.gameslyst.dslist.dto.GameDTO;
 import com.gameslyst.dslist.dto.GameMinDTO;
-import com.gameslyst.dslist.entities.Game;
 import com.gameslyst.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,13 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findByID(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- src/main/java/com/gameslyst/dslist/services/GameService.java
+++ src/main/java/com/gameslyst/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.gameslyst.dslist.services;
 
+import com.gameslyst.dslist.dto.GameDTO;
 import com.gameslyst.dslist.dto.GameMinDTO;
 import com.gameslyst.dslist.entities.Game;
 import com.gameslyst.dslist.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +16,20 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("The 'id' must not be null");
+        }
+
+        return gameRepository.findById(id)
+                .map(GameDTO::new)
+                .orElseThrow(() -> new EntityNotFoundException("Game not found with id: " + id));
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).toList();
+        return result.stream().map(GameMinDTO::new).toList();
     }
 }
